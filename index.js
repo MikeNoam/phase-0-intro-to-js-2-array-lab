6e51dbe58fbf8dfba8c86345060f6ab10bc38f23
MichaelHarris <mike.n.harris1@gmail.com>
2023-01-19 19:58:36 -0500

    success

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+function appendCat(name) {
+         const AllCats = [...cats, name];
+        return(AllCats);
+    }
+function prependCat(name) {
+    const NewCat = [name, ...cats];
+    return(NewCat);
+}
+function removeLastCat() {
+    const CopyCat = [...cats];
+    CopyCat.pop();
+    return(CopyCat); 
+}
+function removeFirstCat() {
+    const CopyCat2 = [...cats];
+    CopyCat2.shift();
+    return(CopyCat2);
+}
